3cc3e5115db517682b2b80e53603f16de99acd95
Eric Grube <grubeeric79@gmail.com>
2016-05-31 15:58:32 -0400

    intersection refactoring

--- src/Intersection.java
+++ src/Intersection.java
@@ -1,21 +1,11 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Intersection {
-	
-	public static void main(String[] args){
-		int[] arr1 = {1};
-		int[] arr2 = {1, 1};
-		int[] arr = intersection(arr1, arr2);
-		for(int i : arr){
-			System.out.println(i);
-		}
-	}
 
-	public static int[] intersection(int[] nums1, int[] nums2) {
+	public int[] intersection(int[] nums1, int[] nums2) {
 		Set<Integer> set = new HashSet<Integer>();
 		List<Integer> intersection = new ArrayList<Integer>();
 		
