dc7b3de8fc82ec5412348db689d186361a25c9d4
Eric Grube <grubeeric79@gmail.com>
2016-06-01 15:49:43 -0400

    Invert Binary Tree

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InvertBinaryTree {
+	
+	public class TreeNode{
+		int val;
+		TreeNode left;
+		TreeNode right;
+		
+		TreeNode(int x){
+			val = x;
+		}
+		
+	}
+	
+	public TreeNode invertTree(TreeNode root){
+		if(root == null)
+			return null;
+		
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		while(!queue.isEmpty()){
+			TreeNode node = queue.poll();
+			TreeNode temp = node.left;
+			node.left = node.right;
+			node.right = temp;
+			
+			if(node.left != null)
+				queue.offer(node.left);
+			if(node.right != null)
+				queue.offer(node.right);
+		}
+		return root;
+	}
+}
