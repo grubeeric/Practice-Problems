d4c89dd703d021f6a1a146b5800699f738892bf1
Eric Grube <grubeeric79@gmail.com>
2016-06-09 15:30:31 -0400

    Print all permutations of a string

--- src/GeneratePermutations.java
+++ src/GeneratePermutations.java
@@ -1,4 +1,22 @@
 
 public class GeneratePermutations {
 
+	public static void main(String[] args){
+		generatePermutation("abcd");
+	}
+	
+	public static void generatePermutation(String str){
+		generatePermutation("", str);
+	}
+	
+	public static void generatePermutation(String prefix, String str){
+		int n = str.length();
+		if(n == 0) System.out.println(prefix);
+		else{
+			for(int i = 0; i < n; i++){
+				generatePermutation(prefix +str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
+			}
+		}
+	}
+	
 }
